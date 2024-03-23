a3a2389463f466c14a7c184d66fdfd9b3115ee98
Kris <109919530+maleker86@users.noreply.github.com>
2024-03-22 20:45:14 -0700

    Renaming Functions for Clarity. NO BUGS

--- main.js
+++ main.js
@@ -600,7 +600,7 @@ function Person(first, last, age, eye) {
         this.taskDescription = description;
   }
 
-  function addTask(task,project) {
+  function addTaskToProject(task,project) {
     this.task = task;
     this.project = project;
     console.log("we want to add: ",task);
@@ -609,14 +609,14 @@ function Person(first, last, age, eye) {
   }
 
   let project1 = new Project("First project");
-  let task1 = new Task("Task1","more words");
-  let task2 = new Task("Task2","lets get some words");
+  let task1 = new Task("Task1","description 1");
+  let task2 = new Task("Task2","description 2 that's longer");
 
   console.log("before: ", project1);
-  console.log("to add: ", task1);
+  console.log("to add: ", task1, task2);
 
-  addTask(task1,project1);
-  addTask(task2,project1);
+  addTaskToProject(task1,project1);
+  addTaskToProject(task2,project1);
 
   console.log("results in: ",project1);
 
