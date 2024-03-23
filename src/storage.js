b5732e612c0c54dc0fc5c76de41d82cb5b48fc3f
Kris <109919530+maleker86@users.noreply.github.com>
2024-03-22 23:09:21 -0700

    Allow editing and creating of proj obj keys. NO BUGS SOMEHOW

--- src/storage.js
+++ src/storage.js
@@ -18,19 +18,47 @@ function Person(first, last, age, eye) {
   function addTaskToProject(task,project) {
     this.task = task;
     this.project = project;
-    console.log("we want to add: ",task);
+    console.log("this time we want to add: ",task);
     console.log("maybe the task array is: ",project.tasks);
     project.tasks.push(task);
+  }
+
+//   thank you sentry https://sentry.io/answers/how-can-i-add-a-key-value-pair-to-a-javascript-object/
+// https://sentry.io/answers/string-contains-substring-javascript/
+//   const obj = { name: "Ben" };
+//   const prop1 = "prop name";
+//   obj[prop1] = "value 1";
+//   console.log(obj);
+//   // {"name": "Ben", "prop name": "value 1", [...]}
+
+  function editTask(taskName,property,new_data) {
+    // this.taskName = taskName;
+    const property_name = property;
+    if (property_name.includes(" ")) {
+        throw "editTask Assign Error: No spaces permitted for object keys.";
+    };
+    // this.new_data = new_data;
+
+    taskName[property_name] = new_data;
   }
 
   let project1 = new Project("First project");
   let task1 = new Task("Task1","description 1");
   let task2 = new Task("Task2","description 2 that's longer");
 
+  console.log("I am trying to edit:",task1);
+  editTask(task1,"priority","high");
+  console.log("please update: ",task1);
+
+  console.log("I am trying to edit:",task2);
+  editTask(task2,"due_date","12/24/2024");
+  console.log("please update: ",task2);
+
   console.log("before: ", project1);
-  console.log("to add: ", task1, task2);
+  console.log("overall to add: ", task1, task2);
 
   addTaskToProject(task1,project1);
   addTaskToProject(task2,project1);
+
 
   console.log("results in: ",project1);
