aa920f67b52dc104e220ba06d8b49779f3e32416
Kris <109919530+maleker86@users.noreply.github.com>
2024-03-24 13:52:56 -0700

    Remove Task Array & allow Editing Tasks

--- main.js
+++ main.js
@@ -598,12 +598,12 @@ function Task(title, description) {
 
 function addTaskToProject(task, project) {
   // console.log("old task array: ",task_arr);
-  this.task = task_arr.find((item) => item.taskTitle === task);
+  // this.task = task_arr.find((item) => item.taskTitle === task);
   this.project = project_arr.find((item) => item.projectTitle === project);
 
   console.log("the project this will be added to is: ", this.project);
-  console.log("we are adding ", this.task, "to ", this.project);
-  this.project.tasks.push(this.task);
+  console.log("we are adding ", task, "to ", this.project);
+  this.project.tasks.push(task);
 
   // console.log("new old array: ", task_arr);
   // task_arr.pop(this.taskName);
@@ -618,19 +618,17 @@ function addProjectToList(project) {
 
 function editTask(projectName, taskName, propertyName, new_data) {
   // console.log("the project array is: ", project_arr);
-  this.project = project_arr.find(
-    (proj_item) => proj_item.projectTitle === projectName,
-  );
-  this.task = project.tasks.find(
-    (task_item) => task_item.taskTitle === taskName,
-  );
-
+  let project = project_arr.find((proj_item) => proj_item.projectTitle === projectName);
+  let task = project.tasks.find((task_item) => task_item.taskTitle === taskName);
+  
   // console.log("the task is ",this.task);
+
+  task.projectName;
 
   if (propertyName.includes(" ")) {
     throw "editTask Assign Error: No spaces permitted for object keys.";
   }
-  this.task[propertyName] = new_data;
+  task[propertyName] = new_data;
 }
 
 function createProject(title) {
@@ -638,26 +636,28 @@ function createProject(title) {
   addProjectToList(project);
 }
 
-function createTask(title, description) {
+function createTask(project,title,description) {
   const task = new Task(title, description);
-  task_arr.push(task);
+  
+  addTaskToProject(task,project);
 }
 
 createProject("Project1");
 createProject("Project2", "these are words");
 
-createTask("Task1", "description 1");
-createTask("Task2", "description 2 that's longer");
+// createTask("Task1", "description 1");
+// createTask("Task2", "description 2 that's longer");
 
-addTaskToProject("Task1", "Project1");
-addTaskToProject("Task2", "Project2");
+createTask("Project1","Task1", "description 1");
+createTask("Project1","Task2", "description 2 that's longer");
+createTask("Project2","Task2",null);
 
 console.log(project_arr);
 
 editTask("Project1","Task1", "priority", "high");
-editTask("Project1","Task1","testing",100);
+editTask("Project1","Task2","testing",100);
 editTask("Project2","Task2", "due_date", "12/24/2024");
-editTask("Project2","Task2", "testing",100);
+editTask("Project2","Task2", "testing", 100);
 
 //----- notes --------------------------
 //   thank you sentry https://sentry.io/answers/how-can-i-add-a-key-value-pair-to-a-javascript-object/
