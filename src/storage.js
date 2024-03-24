e7221e404d74bb01d7cdf7f4e31752b269d68762
Kris <109919530+maleker86@users.noreply.github.com>
2024-03-24 14:35:34 -0700

    Toggle Completed on Tasks. No BUGS

--- src/storage.js
+++ src/storage.js
@@ -9,36 +9,31 @@ function Project(title) {
 function Task(title, description) {
   this.taskTitle = title;
   this.taskDescription = description;
+  this.completed = false;
 }
 
 function addTaskToProject(task, project) {
-  // console.log("old task array: ",task_arr);
-  // this.task = task_arr.find((item) => item.taskTitle === task);
   this.project = project_arr.find((item) => item.projectTitle === project);
 
-  console.log("the project this will be added to is: ", this.project);
-  console.log("we are adding ", task, "to ", this.project);
+  // console.log("the project this will be added to is: ", this.project);
+  // console.log("we are adding ", task, "to ", this.project);
   this.project.tasks.push(task);
-
-  // console.log("new old array: ", task_arr);
-  // task_arr.pop(this.taskName);
-  // console.log("new task array: ", task_arr);
 }
 
 function addProjectToList(project) {
   this.project = project;
   project_arr.push(project);
-  // console.log("the project array is: ", project_arr);
 }
 
 function editTask(projectName, taskName, propertyName, new_data) {
-  // console.log("the project array is: ", project_arr);
-  let project = project_arr.find((proj_item) => proj_item.projectTitle === projectName);
-  let task = project.tasks.find((task_item) => task_item.taskTitle === taskName);
-  
-  // console.log("the task is ",this.task);
+  let project = project_arr.find(
+    (proj_item) => proj_item.projectTitle === projectName,
+  );
+  let task = project.tasks.find(
+    (task_item) => task_item.taskTitle === taskName,
+  );
 
-  task.projectName;
+  task.propertyName;
 
   if (propertyName.includes(" ")) {
     throw "editTask Assign Error: No spaces permitted for object keys.";
@@ -51,28 +46,52 @@ function createProject(title) {
   addProjectToList(project);
 }
 
-function createTask(project,title,description) {
+function createTask(project, title, description) {
   const task = new Task(title, description);
-  
-  addTaskToProject(task,project);
+
+  addTaskToProject(task, project);
+}
+
+function completeTask(projectName,taskName) {
+  let project = project_arr.find(
+    (proj_item) => proj_item.projectTitle === projectName
+  );
+  let task = project.tasks.find(
+    (task_item) => task_item.taskTitle === taskName
+  );
+
+  // console.log(project,task);
+
+  (function checkCompleted() {
+    if (task.completed === true) {
+      completed = false;
+    } else {
+      completed = true;
+    }
+  })();
+
+  editTask("Project1","Task1","completed",completed);
+
+  // console.log(project,task);
+
 }
 
 createProject("Project1");
 createProject("Project2", "these are words");
 
-// createTask("Task1", "description 1");
-// createTask("Task2", "description 2 that's longer");
-
-createTask("Project1","Task1", "description 1");
-createTask("Project1","Task2", "description 2 that's longer");
-createTask("Project2","Task2",null);
+createTask("Project1", "Task1", "description 1");
+createTask("Project1", "Task2", "description 2 that's longer");
+createTask("Project2", "Task2", null);
 
 console.log(project_arr);
 
-editTask("Project1","Task1", "priority", "high");
-editTask("Project1","Task2","testing",100);
-editTask("Project2","Task2", "due_date", "12/24/2024");
-editTask("Project2","Task2", "testing", 100);
+editTask("Project1", "Task1", "priority", "high");
+editTask("Project1", "Task2", "testing", 100);
+editTask("Project2", "Task2", "due_date", "12/24/2024");
+editTask("Project2", "Task2", "testing", 100);
+
+completeTask("Project1", "Task1");
+completeTask("Project1", "Task1");
 
 //----- notes --------------------------
 //   thank you sentry https://sentry.io/answers/how-can-i-add-a-key-value-pair-to-a-javascript-object/
